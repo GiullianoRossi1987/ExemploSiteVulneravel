8989169e7ea31262ee72f5369863be15a38cfd30
Giulliano Rossi <giulliano.scatalon.rossi@gmail.com>
2021-11-23 13:35:08 -0300

    Finalizada alteração de login
    Trocado uso do mysql query pelo prepared statement

--- php/login.php
+++ php/login.php
@@ -5,11 +5,11 @@
     $login = $_POST['usuario'];
     $stmt = $conexao->prepare("SELECT * FROM tb_admin WHERE nm_admin = ? AND ds_senha = ?");
     $stmt->bind_param('ss', $login, $senha);
-    echo $stmt->execute();
+    $stmt->execute();
     // LEGACY: $query = "SELECT * FROM tb_admin WHERE nm_admin ='$login' AND ds_senha = '$senha'";
     // $result = mysqli_query($conexao, $query);
-    echo $query;
-    if(mysqli_num_rows($result)==1){
+    // echo $query;
+    if(mysqli_num_rows($stmt->get_result)==1){
         $_SESSION['acesso'] = "admin";
         header('location:../inicio.php');
     }else{
