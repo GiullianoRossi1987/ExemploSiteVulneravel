7d8273b738e7ee05f10a296aec85ee163033a210
Giulliano Rossi <giulliano.scatalon.rossi@gmail.com>
2021-11-23 12:26:54 -0300

    Alteração de Segurança login. (parte 1)
    Remoção do código direto ao SQL por um statement

--- php/login.php
+++ php/login.php
@@ -3,9 +3,11 @@
     session_start();
     $senha = $_POST['senha'];
     $login = $_POST['usuario'];
-    
-    $query = "SELECT * FROM tb_admin WHERE nm_admin ='$login' AND ds_senha = '$senha'";
-    $result = mysqli_query($conexao, $query);
+    $stmt = $conexao->prepare("SELECT * FROM tb_admin WHERE nm_admin = ? AND ds_senha = ?");
+    $stmt->bind_param('ss', $login, $senha);
+    echo $stmt->execute();
+    // LEGACY: $query = "SELECT * FROM tb_admin WHERE nm_admin ='$login' AND ds_senha = '$senha'";
+    // $result = mysqli_query($conexao, $query);
     echo $query;
     if(mysqli_num_rows($result)==1){
         $_SESSION['acesso'] = "admin";
